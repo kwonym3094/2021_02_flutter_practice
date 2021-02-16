09c183f4049aeae4523f2e77007bc6fad46ea9de
kwonym3094 <1329417@donga.ac.kr>
2021-02-16 17:56:02 +0900

    2일차 : 실제 유용하게 사용할 tab layout 템플릿 작성

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,134 +1,64 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter_example/page/chart_page.dart';
+import 'package:flutter_example/page/main_page.dart';
+import 'package:flutter_example/page/search_page.dart';
+import 'package:flutter_example/page/settings_page.dart';
+import 'package:flutter_example/widget/tabbar_cupertino_widget.dart';
+import 'package:flutter_example/widget/tabbar_material_widget.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Test',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(color: Colors.grey[500]),
-              ),
-            ],
-          ),
-        ),
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41')
-      ],
-    ),
-  );
-
-
   @override
   Widget build(BuildContext context) {
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButton(color, Icons.call, 'CALL'),
-          _buildButton(color, Icons.near_me, 'ROUTE'),
-          _buildButton(color, Icons.share, 'SHARE'),
-          _buildButton(color, Icons.favorite_border, 'LIKE'),
-          _buildButton(color, Icons.add, 'MORE'),
-        ],
-      ),
-    );
-
-    Widget textSection = Container(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
     return MaterialApp(
-      title: 'Flutter Layout Demo',
-      home: Scaffold(
-        body: ListView(
-          children: [
-            Image.asset(
-                'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.fill,
-            ),
-            titleSection,
-            buttonSection,
-            textSection
-          ],
-        ),
-      ),
-    );
-
-
-// non-material design example
-
-/*    return Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Center(
-        child: Text(
-          'Hello World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.black87,
-          ),
-        ),
-      ),
-    );
-*/
-  }
-
-  Column _buildButton(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          padding: EdgeInsets.only(top: 8),
-          child: Icon(
-            icon,
-            color: color,
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-                color: color, fontSize: 12, fontWeight: FontWeight.w400),
-          ),
-        )
-      ],
+        title: 'Flutter Layout Demo',
+        home: PageChanger()
     );
   }
 }
+
+class PageChanger extends StatefulWidget {
+  @override
+  _PageChangerState createState() => _PageChangerState();
+}
+
+class _PageChangerState extends State<PageChanger> {
+
+  int index = 0;
+
+  final pages = <Widget>[
+    MainPage(),
+    SearchPage(),
+    ChartPage(),
+    SettingsPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true, // floating button 뒤로 contents 보이게 함
+      body: pages[index],
+      bottomNavigationBar: TabBarCupertinoWidget(
+        index : index,
+        onChangedTab : onChangedTab,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow),
+        onPressed: () => print('test'),
+        elevation: 0,
+        highlightElevation: 0,
+      ),
+      floatingActionButtonLocation:
+      FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  void onChangedTab(int index){
+    setState(() {
+      this.index = index;
+    });
+  }
+}
+
