027a6ab568d3c1d798b4e05c2af743812a696f82
kwonym3094 <1329417@donga.ac.kr>
2021-02-16 12:20:50 +0900

    2일차 : layout 조정 및 개념 이해

--- flutter_example/README.md
+++ flutter_example/README.md
@@ -1,6 +1,6 @@
 # flutter 보면서 배운 것들
 
-Widget : Android 의 View 와 비슷한 개념
+1. Widget : Android 의 View 와 비슷한 개념
  - 차이점
    * immutable
    * 변해야 하기 전까지 존재함 (android 의 life cycle 과 같지 않음)
@@ -9,7 +9,7 @@ Widget : Android 의 View 와 비슷한 개념
 - StatefulWidget : 통신, UI에 따라 변해야 할 때 사용하기 좋음
   -> 사실은 StatelessWidget 와 동작이 같으나 안에 있는 State 객체가 변함
 
-layout은 어디에 적나?
+2. layout은 어디에 적나?
   - 따로 xml 없음
   - widget tree 라고 하는 양식을 따라 작성
   ex)
@@ -31,4 +31,19 @@ layout은 어디에 적나?
     );
   }
 
-Scaffold : default app 화면
+3. Scaffold : default android app 화면 (material design)
+
+4. Container : padding, margin, border, background color 설정하기 위해 사용
+ - padding: EdgeInsets.fromLTRB(20, 30, 20, 20), << 이런 방식
+
+5. Expanded : 화면 사이즈 맞추기
+  - (property) flex : 크기 비율 설정하는 property 
+6. mainAxisAlignment, crossAxisAlignment : 축 진행 방향에 따라 달라지는 것 잘 이해하기
+
+7. 반응형 앱 만들기 위해 flutter에서 권고하는 사항
+    1) LayoutBuilder 클래스 사용
+    2) build function 안에서 MediaQuery.of() 메소드 사용
+    - 참고 https://flutter.dev/docs/development/ui/layout/responsive
+
+!!!!!
+8. Stateful Widget : non-interactive 한 widget 을 interactive 하게 만들어줌
--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
               Container(
                 padding: const EdgeInsets.only(bottom: 8),
                 child: Text(
-                  'Oeschinen Lake Campground',
+                  'Oeschinen Lake Test',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                   ),
@@ -37,29 +38,97 @@ class MyApp extends StatelessWidget {
     ),
   );
 
+
   @override
   Widget build(BuildContext context) {
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButton(color, Icons.call, 'CALL'),
+          _buildButton(color, Icons.near_me, 'ROUTE'),
+          _buildButton(color, Icons.share, 'SHARE'),
+          _buildButton(color, Icons.favorite_border, 'LIKE'),
+          _buildButton(color, Icons.add, 'MORE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter Layout Demo',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Layout Demo'),
-        ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+                'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.fill,
+            ),
             titleSection,
+            buttonSection,
+            textSection
           ],
         ),
       ),
     );
+
+
+// non-material design example
+
+/*    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+    );
+*/
   }
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
+  Column _buildButton(Color color, IconData icon, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 8),
+          child: Icon(
+            icon,
+            color: color,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                color: color, fontSize: 12, fontWeight: FontWeight.w400),
+          ),
+        )
+      ],
     );
   }
-
-
 }
