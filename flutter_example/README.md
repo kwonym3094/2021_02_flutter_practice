9cb6d12df2234e2e60eb69974f67420e4e409997
kwonym3094 <1329417@donga.ac.kr>
2021-02-17 16:56:48 +0900

    3일차 : 레이아웃 연습 및 네비게이터 연습

--- flutter_example/README.md
+++ flutter_example/README.md
@@ -5,9 +5,14 @@
    * immutable
    * 변해야 하기 전까지 존재함 (android 의 life cycle 과 같지 않음)
 
-- StatelessWidget : 아무것도 변하지 않을 때 사용하기 좋음 (ex. logo image)
-- StatefulWidget : 통신, UI에 따라 변해야 할 때 사용하기 좋음
+ - StatelessWidget : 아무것도 변하지 않을 때 사용하기 좋음 (ex. logo image, icon button, text ...)
+ - StatefulWidget : 사용자와 상호작용(터치, 스와이프, 통신 등)에 따라 변해야 할 때 사용 (ex. checkbox, radio, slider, inkwell, form, text field)
   -> 사실은 StatelessWidget 와 동작이 같으나 안에 있는 State 객체가 변함
+  -> State 객체에 위젯의 상태가 저장됨
+  -> State는 변할 수 있는 values 들로 구성됨
+  -> 위젯의 상태가 변할때 setState()를 call 하게 됨
+  -> setState()는 프레임워크에 위젯의 state가 변했으니 다시 그리라고 알려줌
+  -> State class 안에 변할 수 있는 데이터를 담게 됨 ex. bool _isFavorited = true; int _favoriteCount = 41;
 
 2. layout은 어디에 적나?
   - 따로 xml 없음
@@ -47,3 +52,18 @@
 
 !!!!!
 8. Stateful Widget : non-interactive 한 widget 을 interactive 하게 만들어줌
+
+9. IconButton 과 Icon의 차이 : IconButton 은 onPressed 라는 callback function 가지고 있음
+
+10. SizedBox 안에 Text를 넣으면 State 변할 때 값이 제대로 변하지 않는 것을 방지할 수 있음
+
+11. state 관리는 어떻게 해야하는가?
+ - 3가지 방법 있음
+  * 위젯이 스스로의 state 관리
+  * 부모가 위젯의 state 관리
+  * 믹스 앤 매치 : stateful widget 자체가 몇개의 state를 관리하고 부모가 몇개의 state를 관리
+ - 심미적인 부분(애니메이션 등등)에 관련된 것은 위젯이 스스로 관리
+ - 사용자 데이터와 관련된 것(체크박스, 슬라이더 위치) 등은 부모가 관리
+ 
+12. 플러터에서는 screen 이나 page 를 route 라고 부름
+13. 이 route 간 이동할 수 있게 만들어 주는 것 -> navigator
--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,134 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter_example/navigator_test.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'Named Routes Demo',
+    // Start the app with the "/" named route. In this case, the app starts
+    // on the FirstScreen widget.
+    initialRoute: '/',
+    routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      '/': (context) => FirstScreen(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/second': (context) => SecondScreen(),
+    },
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Test',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(color: Colors.grey[500]),
-              ),
-            ],
-          ),
-        ),
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41')
-      ],
-    ),
-  );
-
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButton(color, Icons.call, 'CALL'),
-          _buildButton(color, Icons.near_me, 'ROUTE'),
-          _buildButton(color, Icons.share, 'SHARE'),
-          _buildButton(color, Icons.favorite_border, 'LIKE'),
-          _buildButton(color, Icons.add, 'MORE'),
-        ],
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('title'),
       ),
-    );
-
-    Widget textSection = Container(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
-    return MaterialApp(
-      title: 'Flutter Layout Demo',
-      home: Scaffold(
-        body: ListView(
-          children: [
-            Image.asset(
-                'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.fill,
-            ),
-            titleSection,
-            buttonSection,
-            textSection
-          ],
-        ),
-      ),
-    );
-
-
-// non-material design example
-
-/*    return Container(
-      decoration: BoxDecoration(color: Colors.white),
       child: Center(
-        child: Text(
-          'Hello World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.black87,
-          ),
+        child: ElevatedButton(
+          child: Text('Launch screen'),
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamed(context, '/second');
+          },
         ),
       ),
-    );
-*/
-  }
-
-  Column _buildButton(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          padding: EdgeInsets.only(top: 8),
-          child: Icon(
-            icon,
-            color: color,
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-                color: color, fontSize: 12, fontWeight: FontWeight.w400),
-          ),
-        )
-      ],
     );
   }
 }
