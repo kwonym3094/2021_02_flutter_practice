86e2cf95fab26389aa421087a7752b1fad7e9a7a
kwonym3094 <kwonym3094@gmail.com>
2021-04-29 22:25:43 +0900

    [참고] extension 패키지

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,171 +1,61 @@
-import 'package:flash/flash.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+class StringUtil {
+  static bool isValidEmail(String str) {
+    final emailRegExp = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+    return emailRegExp.hasMatch(str);
+  }
+}
+
+// the code above is unnatural => we can change this with "extension" keyword!
+
+// it makes developers extend the class created by others like String, int
+
+extension StringExtensions on String {
+  bool get isValidEmail {
+    final emailRegExp = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+    return emailRegExp.hasMatch(this);
+  }
+
+  String concatWithSpace(String other) {
+    return '$this $other';
+  }
+
+  // how to define custom operator
+  String operator &(String other) => '$this $other';
+}
+
+// extension IntExtensions on int {
+//   int get addTen => this+10;
+// }
+
+// extension DoubleExtensions on double{
+//   double get addTehn => this+10;
+// }
+// => just a simple duplication of IntExtensions !
+
+// extension NumExtensions on num{
+// //   num get addTen => this+10;
+//   // this will occur runtime error!
+//   // needs generic type!
+//   // =>
+// }
+
+extension NumGenericExtensions<T extends num> on T {
+  T addTen() => (this + 10) as T;
+}
 
 void main() {
-  runApp(MyApp());
-}
+  dynamic output = "email@gmail.com".isValidEmail;
+  print(output);
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: HomePage(),
-    );
-  }
-}
+  dynamic output2 = 'first'.concatWithSpace("trial");
+  print(output2);
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          child: Text('Go to the Flash page'),
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => Overlay(initialEntries: [
-                  OverlayEntry(
-                    builder: (context) => FlashPage(),
-                  )
-                ]),
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
+  dynamic output3 = 'first' & "trial";
+  print(output3);
 
-class FlashPage extends StatefulWidget {
-  @override
-  _FlashPageState createState() => _FlashPageState();
-}
+  int output4 = 1.addTen();
+  print(output4);
 
-class _FlashPageState extends State<FlashPage> {
-  bool _isToastShown = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            RaisedButton(
-              child: Text('Show Toast'),
-              onPressed: () async {
-                if (_isToastShown) return;
-
-                _isToastShown = true;
-
-                await showFlash(
-                    context: context,
-                    duration: const Duration(seconds: 4),
-                    builder: (context, controller) {
-                      return Flash.dialog(
-                          alignment: Alignment.bottomCenter,
-                          margin: const EdgeInsets.only(bottom: 48.0),
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(8)),
-                          backgroundGradient: LinearGradient(
-                              colors: [Colors.indigo, Colors.deepPurple]),
-                          controller: controller,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              "테스트",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ));
-                    });
-
-                _isToastShown = false;
-              },
-            ),
-            RaisedButton(
-              child: Text('Show Snackbar'),
-              onPressed: () {
-                showFlash(
-                    context: context,
-                    duration: const Duration(seconds: 4),
-                    persistent: false,
-                    builder: (context, controller) {
-                      return Flash.bar(
-                          margin: const EdgeInsets.only(
-                              top: 15, left: 10, right: 10),
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(8)),
-                          controller: controller,
-                          position: FlashPosition.top,
-                          enableDrag: true,
-                          horizontalDismissDirection:
-                              HorizontalDismissDirection.horizontal,
-                          backgroundGradient: LinearGradient(colors: [
-                            Colors.yellow,
-                            Colors.amber,
-                          ]),
-                          forwardAnimationCurve: Curves.easeOutBack,
-                          reverseAnimationCurve: Curves.slowMiddle,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: FlashBar(
-                              primaryAction: IconButton(
-                                icon: Icon(Icons.ac_unit),
-                                onPressed: () {},
-                              ),
-                              title: Text(
-                                "You are seeing a snackbar!",
-                                style: Theme.of(context).textTheme.headline6,
-                              ),
-                              message: Text("This is something!!!"),
-                              icon: Icon(
-                                Icons.info,
-                                color: Colors.black,
-                              ),
-                              shouldIconPulse: false,
-                              showProgressIndicator: true,
-                            ),
-                          ));
-                    });
-              },
-            ),
-            RaisedButton(
-              child: Text('Show Dialog'),
-              onPressed: () {
-                showFlash(
-                  context: context,
-                  // A dialog cannot be persistent - must be poppable.
-                  persistent: false,
-                  builder: (context, controller) {
-                    return Flash.dialog(
-                      controller: controller,
-                      borderRadius: const BorderRadius.all(Radius.circular(8)),
-                      margin: const EdgeInsets.all(8),
-                      // Again, FlashBar is a perfect candidate for the child widget.
-                      child: FlashBar(
-                        message:
-                            Text('This FlashBar looks like an AlertDialog.'),
-                        actions: [
-                          FlatButton(
-                            onPressed: () {
-                              controller.dismiss();
-                            },
-                            child: Text('OK'),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  double output5 = 1.0.addTen();
+  print(output5);
 }
