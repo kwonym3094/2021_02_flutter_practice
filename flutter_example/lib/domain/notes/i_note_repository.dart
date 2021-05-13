6b91f618c5cbc9dd448f9f8ba4439e16959e0819
kwonym3094 <kwonym3094@gmail.com>
2021-05-13 12:41:40 +0900

    [스터디] Domain-Driven-Design Pattern 15 Note DTO 생성

--- flutter_example/lib/domain/notes/i_note_repository.dart
+++ flutter_example/lib/domain/notes/i_note_repository.dart
@@ -1,9 +1,23 @@
 // note value object와 repository를 연결하는 interface
+import 'package:dartz/dartz.dart';
+import 'package:flutter_example/domain/notes/note.dart';
+import 'package:flutter_example/domain/notes/note_failure.dart';
+import 'package:kt_dart/kt.dart';
+
 abstract class INoteRepository {
-  // watch notes (firestore 에서만 가능할 듯)
+  // 만들어야할 interface
+  // - watch notes (firestore 에서만 가능할 듯)
+  // - watch uncompleted notes
+  // - CUD (R 부분은 위의 두 개)
 
-  // watch uncompleted notes
+  // R
+  // watch note : 계속 지켜보고 있어야되기때문에 Future이 아니라 Stream
+  Stream<Either<NoteFailure, KtList<Note>>> watchAll();
+  Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted();
 
-  // CUD (R 부분은 위의 두 개)
-
+  // CUD
+  // create는 return 타입이 없으니 Unit으로
+  Future<Either<NoteFailure, Unit>> create(Note note);
+  Future<Either<NoteFailure, Unit>> update(Note note);
+  Future<Either<NoteFailure, Unit>> delete(Note note);
 }
---
+++ flutter_example/lib/infrastructure/notes/note_dtos.dart
@@ -0,0 +1,109 @@
+// DTO(Data Transfer Object)
+
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_example/domain/core/value_objects.dart';
+import 'package:flutter_example/domain/notes/note.dart';
+import 'package:flutter_example/domain/notes/todo_item.dart';
+import 'package:flutter_example/domain/notes/value_objects.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kt_dart/kt.dart';
+
+part 'note_dtos.freezed.dart';
+part 'note_dtos.g.dart'; // DTO를 JSON 으로 변환하기 위해서 작성
+
+@freezed
+abstract class NoteDto implements _$NoteDto {
+  const NoteDto._();
+
+  const factory NoteDto({
+    @JsonKey(ignore: true) String? id,
+    required String body,
+    required int color,
+    required List<TodoItemDto> todos,
+    // 나중에 바꿔야할 값 : firebase의 서버 시간을 따라감
+    required dynamic serverTimeStamp,
+  }) = _NoteDto;
+
+  factory NoteDto.fromDomain(Note note) {
+    return NoteDto(
+      id: note.id.getOrCrash(),
+      body: note.body.getOrCrash(),
+      color: note.color.getOrCrash().value,
+      todos: note.todos
+          .getOrCrash()
+          .map(
+            (todoItem) => TodoItemDto.fromDomain(todoItem),
+          )
+          .asList(),
+      serverTimeStamp: FieldValue.serverTimestamp(),
+    );
+  }
+
+  Note toDomain() {
+    return Note(
+      id: UniqueId.fromUniqueString(id!),
+      body: NoteBody(body),
+      color: NoteColor(Color(color)),
+      todos: List3(todos.map((dto) => dto.toDomain()).toImmutableList()),
+    );
+  }
+
+  factory NoteDto.fromJson(Map<String, dynamic> json) =>
+      _$NoteDtoFromJson(json);
+
+  // firestore을 사용하기 때문에 firestore id가 필요함
+  //  - json에 같이 담겨서 오는것이 아니기 때문에 따로 받아줘야함
+  factory NoteDto.fromFirestore(DocumentSnapshot doc) {
+    return NoteDto.fromJson(doc.data()! as Map<String, dynamic>)
+        .copyWith(id: doc.id);
+  }
+}
+
+// json serializer가 FieldValue(=3rd party library object)를 인식하지 못하기 때문에 converter 가 필요함
+class ServerTimeStampConverter implements JsonConverter<FieldValue, Object> {
+  // 사용할 수 있도록 annotation화 해야함 (dart 에서 annotation화 할때 const 로 정의해줘야하는 것 주의)
+  const ServerTimeStampConverter();
+
+  // JSON -> DTO 에 넣을때
+  @override
+  FieldValue fromJson(Object json) {
+    return FieldValue.serverTimestamp();
+  }
+
+  // 중요!
+  // DTO -> JSON 으로 만들 때
+  @override
+  Object toJson(FieldValue fieldValue) => fieldValue;
+}
+
+@freezed
+abstract class TodoItemDto implements _$TodoItemDto {
+  const TodoItemDto._();
+
+  const factory TodoItemDto({
+    required String id,
+    required String name,
+    required bool done,
+  }) = _TodoItemDto;
+
+  factory TodoItemDto.fromDomain(TodoItem todoItem) {
+    return TodoItemDto(
+      id: todoItem.id.getOrCrash(),
+      name: todoItem.name.getOrCrash(),
+      done: todoItem.done,
+    );
+  }
+
+  TodoItem toDomain() {
+    return TodoItem(
+      id: UniqueId.fromUniqueString(id),
+      name: TodoName(name),
+      done: done,
+    );
+  }
+
+  factory TodoItemDto.fromJson(Map<String, dynamic> json) =>
+      _$TodoItemDtoFromJson(json);
+}
